995e90cbd2167b4a88bd8f1d28298a252c1dc0a0
Onur T <onurtumer@gmail.com>
2018-11-15 21:29:35 +0300

    AddTag form submission functional, FormTitle styling change

--- src/screens/AddTag/AddTag.tsx
+++ src/screens/AddTag/AddTag.tsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { Field, Form, Formik, FormikProps } from "formik"
-import { Mutation } from "react-apollo"
+import { Field, Form, Formik, FormikActions, FormikProps } from "formik"
+import { Mutation, MutationFn } from "react-apollo"
 import { MdLocalOffer as TagIcon, MdArchive as SaveIcon } from "react-icons/md"
+import gql from "graphql-tag"
 
 import styles from "./AddTag.scss"
 import {
@@ -26,51 +27,96 @@ const initialValues: Tag = {
   name: ""
 }
 
+const addTagMuatation = gql`
+  mutation addTag(
+    $name: String!
+    $metaTitle: String
+    $metaDescription: String
+  ) {
+    createTag(
+      name: $name
+      metaTitle: $metaTitle
+      metaDescription: $metaDescription
+    ) {
+      id
+    }
+  }
+`
+
+// TODO: Form Error and Loading Components
+// TODO: Success feedback via Modal
 const AddTag: React.SFC = () => {
+  const handleSubmit = (mutation: MutationFn<null, Tag>) => async (
+    values: Tag,
+    { resetForm, setSubmitting }: FormikActions<Tag>
+  ) => {
+    setSubmitting(true)
+    await mutation({
+      variables: {
+        name: values.name.toLowerCase(),
+        metaDescription: values.metaDescription.toLowerCase(),
+        metaTitle: values.metaDescription.toLowerCase()
+      }
+    })
+    resetForm()
+    setSubmitting(false)
+  }
+
   return (
-    <Shell>
-      <Container narrow="narrow">
-        <Paper className={styles.paper} elevation={2}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={values => console.log(values)}
-            render={() => (
-              <Form>
-                <FormTitle title="Add New Tag" icon={<TagIcon />} />
-                <Field
-                  type="text"
-                  name="name"
-                  placeholder="Tag name"
-                  label="Tag Name"
-                  component={FormInput}
+    <Mutation mutation={addTagMuatation}>
+      {(addTag, { loading, error }) => {
+        return (
+          <Shell>
+            <Container narrow="narrow">
+              <Paper className={styles.paper} elevation={2}>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={handleSubmit(addTag)}
+                  render={(form: FormikProps<Tag>) => {
+                    if (loading) return <p>Loading...</p>
+                    return (
+                      <Form method="post">
+                        <FormTitle title="Add New Tag" icon={<TagIcon />} />
+                        {error && <p>Error: {error.message}</p>}
+                        <Field
+                          type="text"
+                          name="name"
+                          placeholder="Tag name"
+                          label="Tag Name"
+                          component={FormInput}
+                        />
+                        <Field
+                          type="text"
+                          name="metaTitle"
+                          placeholder="Title"
+                          label="Title (Meta)"
+                          component={FormInput}
+                        />
+                        <Field
+                          type="text"
+                          name="metaDescription"
+                          placeholder="Description"
+                          label="Description (Meta)"
+                          component={FormInput}
+                        />
+                        <Button
+                          text="Save Tag"
+                          type="submit"
+                          success={true}
+                          disabled={form.isSubmitting}
+                          icon={<SaveIcon />}
+                        />
+                      </Form>
+                    )
+                  }}
                 />
-                <Field
-                  type="text"
-                  name="metaTitle"
-                  placeholder="Title"
-                  label="Title (Meta)"
-                  component={FormInput}
-                />
-                <Field
-                  type="text"
-                  name="metaDescription"
-                  placeholder="Description"
-                  label="Description (Meta)"
-                  component={FormInput}
-                />
-                <Button
-                  text="Save Tag"
-                  type="submit"
-                  success={true}
-                  icon={<SaveIcon />}
-                />
-              </Form>
-            )}
-          />
-        </Paper>
-      </Container>
-    </Shell>
+              </Paper>
+            </Container>
+          </Shell>
+        )
+      }}
+    </Mutation>
   )
 }
 
